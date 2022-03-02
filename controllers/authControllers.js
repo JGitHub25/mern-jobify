459907778010b0011e50daedd0dc4b72f3f8ff29
jpeg25 <jairo25509@hotmail.com>
2022-03-02 09:12:58 -0500

    register controller basic functionality.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,18 @@
-const register = (req, res) => {
-  res.send("Register user.");
+import { UserModel } from "../models/UserSchema.js";
+
+const register = async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: "There was an error registering the user." });
+    console.log(error);
+  }
 };
-const login = (req, res) => {
+const login = async (req, res) => {
   res.send("login user.");
 };
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
   res.send("updateUser user.");
 };
 

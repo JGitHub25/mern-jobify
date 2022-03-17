30b83bfe2aeca57e52f6619e506a1cd56bc09d2f
jpeg25 <jairo25509@hotmail.com>
2022-03-17 17:06:59 -0500

    feat: turned dashboard into a protected route with the ProtectedRoute component.

---
+++ client/src/pages/ProtectedRoute.js
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+
+export const ProtectedRoute = ({ children }) => {
+  const { user } = useAppContext();
+
+  if (!user) {
+    return <Navigate to="/landing" />;
+  }
+
+  return children;
+};
